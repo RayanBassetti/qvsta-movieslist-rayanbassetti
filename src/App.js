1f72473f65097bdb84df3a030438dad224816cbe
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2019-04-07 00:04:02 +0600

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Movies from './js/components/Movies.js';
 import Header from './js/components/Header.js';
+import Search from './js/components/Search.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Search />
         <Movies />
       </div>
     );
