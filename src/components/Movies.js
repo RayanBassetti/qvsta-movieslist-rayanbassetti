0895d7f5e3d6f7a778e34797c3269139e18a3c3e
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2019-04-11 00:42:01 +0600

    Removed JQuery, setup for movies genres choices

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import $ from "jquery";
 
 import SingleMovie from './SingleMovie.js';
 import SingleGenre from './SingleGenre.js'
-// import axios from 'axios';
 
 
 const API_KEY = `${process.env.REACT_APP_API_KEY}`
@@ -14,6 +12,7 @@ class Movies extends React.Component {
         super()
         this.state = {
             researchmovie: "",
+            selectedGenre: "",
             allMovies: [],
             allGenres: []
         }
@@ -34,25 +33,30 @@ class Movies extends React.Component {
 
     OnSelect(event) {
         console.log(event.target.value)
+        this.setState({
+            selectedGenre: event.target.value
+        })
+        console.log(this.state.selectedGenre)
+        const selectedGenreId = this.state.selectedGenre
+        this.LaunchSearch(selectedGenreId)
     }
 
-    LaunchSearch(value) {
+    LaunchSearch(value, selectedGenreId) {
+        this.state.allGenres.forEach(genre => {
+            if (selectedGenreId === genre.id) {
+                this.setState({
+                    selectedGenre: selectedGenreId
+                })
+            }
+        })
+        const selectedGenre = this.state.selectedGenre
         axios
-            // const allGenres = this.state.allGenres
-            // allGenres.forEach(genre => {
-            //     if (genre === selectedGenreId) {
-            //         this.setState({
-            //             selectedGenre: genre
-            //         })
-            //     }
-            // })
-            .post(`https://api.themoviedb.org/3/discover/movie?api_key=435ab096a795a0a39c4e7bca5f71fd75&sort_by=popularity.desc&primary_release_year=${value}`)
+            .post(`https://api.themoviedb.org/3/discover/movie?api_key=435ab096a795a0a39c4e7bca5f71fd75&sort_by=popularity.desc&primary_release_year=${value}&with_genres=28`)
             .then(response => {
                 const results = response.data.results
                 const moviesList = []
                 results.forEach(movie => {
-                    console.log(movie.original_title)
-                    const singleMovie = <SingleMovie key={movie.key} movie={movie} />
+                    const singleMovie = <SingleMovie key={movie.id} movie={movie} />
                     moviesList.push(singleMovie)
                 });
 
@@ -96,8 +100,11 @@ class Movies extends React.Component {
                         onChange={this.HandleSearch}
                     >
                     </input>
-                    <select defaultValue="Select a genre">
-                        <option>
+                    <select 
+                        defaultValue="Select a genre" 
+                        onClick={event => this.OnSelect(event)}
+                    >
+                        <option value="null">
                             Select a genre
                         </option>
                         {this.state.allGenres}
