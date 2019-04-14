91a9a5b57e13d1a17cc4fa99939c1a6734b8269c
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2019-04-14 15:46:09 +0600

    Fixed app

--- src/containers/FrontPage.js
+++ src/containers/FrontPage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './FrontPage.scss'
 
-import textUserInput from './textUserInput.js'
-
 export default function FrontPage(props) {
     return (
         <div className="FrontPage">
@@ -24,7 +22,66 @@ export default function FrontPage(props) {
                             {props.state.allGenres}
                         </select>
                     </div>
-                    <textUserInput />
+                    {/* user input for the years  */}
+                    <div className="textInputUser"> 
+                            <input
+                                placeholder="Between 2000..." 
+                                type="text" 
+                                onChange={props.HandleSearch}
+                                name="researchyear1"
+                                defaultValue={props.state.researchyear1}
+                            >
+                            </input>
+                            <p>2) Enter two specific years</p>
+                            <input 
+                                placeholder="... and today." 
+                                type="text" 
+                                onChange={props.HandleSearch}
+                                name="researchyear2"
+                                defaultValue={props.state.researchyear2}
+                            >
+                            </input> 
+                    </div>
+                    {/* user input for the ratings  */}
+                    <div className="textInputUser">
+                            <input
+                                placeholder="From 0..." 
+                                type="text" 
+                                onChange={props.HandleSearch}
+                                name="researchrating1"
+                                defaultValue={props.state.researchrating1}
+                            >
+                            </input>
+                            <p>3) Enter two specific ratings</p>
+                            <input 
+                                placeholder="... to 10." 
+                                type="text" 
+                                onChange={props.HandleSearch}
+                                name="researchrating2"
+                                defaultValue={props.state.researchrating2}
+                            >
+                            </input> 
+                    </div>
+                    {/* user input for the runtime  */}
+                    <div className="textInputUser">
+                            <input
+                                placeholder="Between 60..." 
+                                type="text" 
+                                onChange={props.HandleSearch}
+                                name="researchruntime1"
+                                defaultValue={props.state.researchruntime1}
+                            >
+                            </input>
+                            <p>4) Enter two specific runtimes (in minutes).</p>
+                            <input 
+                                placeholder="... and 240." 
+                                type="text" 
+                                onChange={props.HandleSearch}
+                                name="researchruntime2"
+                                defaultValue={props.state.researchruntime2}
+                            >
+                            </input> 
+                    </div>
                     <input type="button" className="launchSearch" onTouch={props.ConfigureSearch} onClick={props.ConfigureSearch} value="Search"></input> 
                     
             </form>
